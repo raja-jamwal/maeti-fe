8acbc94a2bd813d5dea60521e6e16460a1bbbb33
Raja Jamwal <linux1@zoho.com>
2019-07-26 23:57:37 +0530

    ConnectedProfile new account log

--- src/components/profile-card/connected-profile.tsx
+++ src/components/profile-card/connected-profile.tsx
@@ -3,11 +3,16 @@ import TestProfile from './test-profile';
 import { connect } from 'react-redux';
 import { isEmpty } from 'lodash';
 import { IRootState } from '../../store/index';
+import { getLogger } from '../../utils/logger';
 
 const mapStateToProps = (state: IRootState, ownProps: IProfileProps) => {
 	const accountData = state.account;
 
-	if (isEmpty(accountData)) return {};
+	if (isEmpty(accountData)) {
+		logger.log('dev: empty user account, we probably deleted the account in server restart');
+		logger.log('dev: create a new account');
+		return {};
+	}
 
 	const fallbackUserProfile = accountData.userProfile;
 
@@ -22,9 +27,14 @@ const mapStateToProps = (state: IRootState, ownProps: IProfileProps) => {
 	};
 };
 
-export default connect(
-	mapStateToProps,
-	null
-)(ProfileCard);
+const ConnectedProfile = () =>
+	connect(
+		mapStateToProps,
+		null
+	)(ProfileCard);
+
+const logger = getLogger(ConnectedProfile);
+
+export default ConnectedProfile();
 
 // export default TestProfile;
