a0b1f9046d23edc41eb297111ad4488b85a6e0de
Raja Jamwal <linux1@zoho.com>
2020-06-16 11:28:17 +0530

    ota 10, last message on top, remove interests

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -182,7 +182,7 @@ export default createBottomTabNavigator(
 		ExploreStack,
 		MessagesStack,
 		FavouritesStack,
-		InterestsStack,
+		// InterestsStack,
 		MoreStack
 	},
 	bottomTabNavigatorOptions
