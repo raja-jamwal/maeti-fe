e73e664bdaa222101d5cf569bc983ab0b08cdf17
Raja Jamwal <linux1@zoho.com>
2020-07-05 19:42:52 +0530

    watingText from other side and ota 12

--- src/components/user-channel/index.tsx
+++ src/components/user-channel/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { Image, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Value } from '../text/index';
 import GlobalStyles from '../../styles/global';
 import { Message, UserProfile } from '../../store/reducers/account-defination';
 import { formatDuration } from '../../utils';
 import { isEmpty, head } from 'lodash';
+import { getCurrentUserProfileId } from '../../store/reducers/account-reducer';
 import { isProfileBlocked } from '../../store/reducers/user-profile-reducer';
 import { IRootState } from '../../store/index';
 import { connect } from 'react-redux';
@@ -13,14 +14,16 @@ interface IUserChannelProps {
 	userProfile: UserProfile;
 	latestMessage: Message;
 	isProfileBlocked: boolean;
+	currentProfileId: number;
 }
 
 class UserChannel extends React.Component<IUserChannelProps> {
 	render() {
-		const { userProfile, latestMessage, isProfileBlocked } = this.props;
+		const { userProfile, latestMessage, isProfileBlocked, currentProfileId } = this.props;
 		if (isProfileBlocked) return null;
 		const userProfileImage =
 			(!isEmpty(userProfile.photo) && head(userProfile.photo).url) || undefined;
+		const isLatestMessageFromOther = latestMessage.fromUser.id !== currentProfileId;
 		return (
 			<View style={[GlobalStyles.row, GlobalStyles.alignCenter, styles.container]}>
 				<View style={GlobalStyles.paddedRight}>
@@ -28,9 +31,12 @@ class UserChannel extends React.Component<IUserChannelProps> {
 				</View>
 				<View style={GlobalStyles.expand}>
 					<View style={GlobalStyles.row}>
-						<Value style={[GlobalStyles.bold, GlobalStyles.expand]}>
-							{userProfile.fullName}
-						</Value>
+						<Value style={[GlobalStyles.bold]}>{userProfile.fullName}</Value>
+						{isLatestMessageFromOther && (
+							<View style={styles.waitingTextContainer}>
+								<Value style={styles.waitingText}>WAITING</Value>
+							</View>
+						)}
 					</View>
 					{latestMessage && (
 						<View style={GlobalStyles.row}>
@@ -52,11 +58,25 @@ const styles = StyleSheet.create({
 		width: 40,
 		height: 40,
 		borderRadius: 20
+	},
+	waitingTextContainer: {
+		backgroundColor: '#ccc',
+		borderRadius: 2
+	},
+	waitingText: {
+		fontSize: 8,
+		// margin: 2,
+		padding: 4,
+		borderRadius: 2,
+		color: 'white',
+		letterSpacing: 1.3
 	}
 });
 
 const mapStateToProps = (state: IRootState, ownProps: IUserChannelProps) => {
+	const currentProfileId = getCurrentUserProfileId(state);
 	return {
+		currentProfileId,
 		isProfileBlocked: isProfileBlocked(state, ownProps.userProfile.id)
 	};
 };
