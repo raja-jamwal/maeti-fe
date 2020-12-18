29f0548c461b887f605c870c76fa714a51d17e5a
Raja Jamwal <linux1@zoho.com>
2020-12-18 15:28:00 +0530

    stomp reconnect

--- src/store/middleware/rtm-middleware.ts
+++ src/store/middleware/rtm-middleware.ts
@@ -28,47 +28,53 @@ export const connectRTM = createAction(ACTIONS.RTM_CONNECT);
 
 export const rtmMiddleware = store => next => action => {
 	const logger = getLogger(rtmMiddleware);
+	const startRTM = () => {
+		logger.log('Trying to start RTM');
+		client = Stomp.Stomp.over(function sockFactory() {
+			return new SockJS(API.RTM.CONNECT);
+		});
+		client.heartbeat.outgoing = 1000;
+		client.reconnect_delay = 1000;
+		client.heartbeat.incoming = 1000;
+		client.connect({}, function(frame) {
+			logger.log('RTM connected');
+			if (!client) {
+				logger.log('No WS client, closing');
+				return;
+			}
+
+			const currentProfileId = getCurrentUserProfileId(store.getState());
+			if (!currentProfileId) {
+				logger.log('Cant find currentProfileId');
+				return;
+			}
+
+			logger.log(`Subscribing rtm updates for profileId ${currentProfileId}`);
+
+			client.subscribe('/topic/' + currentProfileId, function(data) {
+				try {
+					const text = (data.body || '{}').trim();
+					const message = JSON.parse(text);
+					const type = message.eventType;
+					if (!type) {
+						logger.log('invalid rtm message type');
+						return;
+					}
+					logger.log('RTM message', type);
+					const payload = message.payload;
+					if (typeof handlers[type] === 'function') {
+						handlers[type](store, payload);
+					}
+				} catch (er) {
+					logger.log(er);
+				}
+			});
+		});
+	}
 
 	switch (action.type) {
 		case ACTIONS.RTM_CONNECT:
-			// socket = new SockJS(API.RTM.CONNECT);
-			client = Stomp.Stomp.over(function sockFactory() {
-				return new SockJS(API.RTM.CONNECT);
-			});
-			client.connect({}, function(frame) {
-				logger.log('RTM connected');
-				if (!client) {
-					logger.log('No WS client, closing');
-					return;
-				}
-
-				const currentProfileId = getCurrentUserProfileId(store.getState());
-				if (!currentProfileId) {
-					logger.log('Cant find currentProfileId');
-					return;
-				}
-
-				logger.log(`Subscribing rtm updates for profileId ${currentProfileId}`);
-
-				client.subscribe('/topic/' + currentProfileId, function(data) {
-					try {
-						const text = (data.body || '{}').trim();
-						const message = JSON.parse(text);
-						const type = message.eventType;
-						if (!type) {
-							logger.log('invalid rtm message type');
-							return;
-						}
-						logger.log('RTM message', type);
-						const payload = message.payload;
-						if (typeof handlers[type] === 'function') {
-							handlers[type](store, payload);
-						}
-					} catch (er) {
-						logger.log(er);
-					}
-				});
-			});
+			startRTM();
 			break;
 		default:
 			break;
